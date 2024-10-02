157fad2c38dd9e19e1cc6aff7aa38287249219d5
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-10-02 14:00:47 +0000

    Answer

--- PP1_10.py
+++ PP1_10.py
@@ -1,23 +1,30 @@
-
-
+import math
+import numpy as np
 def q1(): 
-  #Write Assignment code here
+  x = float(input("Enter a integer: "))
+  print(math.sqrt(x))
 
 
 def q2(): 
-  #Write Assignment code here
+  y = int(input("Input a number: "))
+  yY = np.float64(y)
+  print(math.isqrt(yY))
 
 
 def q3(): 
-  #Write Assignment code here
+  z = int(input("Input a number: "))
+  zZ = np.float64(z)
+  print(math.trunc(zZ))
 
 
 def q4(): 
-  #Write Assignment code here
+  print(math.ceil(float("Input a number: ")))
 
 
 def q5(): 
-  #Write Assignment code here
+  x = float(input("Input a number: "))
+  y = float(input("Input another number: "))
+  print(math.trunc((x*y)/2))
 
 
 
